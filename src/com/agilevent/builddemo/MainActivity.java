db23cc307460dff97d73e600734b59e5d0e0c1bf
Donn Felker <donn@donnfelker.com>
2010-09-22 08:14:51 -0700

    breaking build in java

--- src/com/agilevent/builddemo/MainActivity.java
+++ src/com/agilevent/builddemo/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+this should break the build
     }
 }
